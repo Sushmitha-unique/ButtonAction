5be4b782118f5fbbd40c97511c59de97312d6e49
B.sushmitha <106570754+Sushmitha-unique@users.noreply.github.com>
2024-01-28 10:59:14 +0530

    ButtonAction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+    const [text,settext]=useState(false)
+return(
+    <div>
+        <button onClick={()=>settext(true)}>ClickMe</button>
+        {
+            text!==false && <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+        }
     </div>
-  );
+)
 }
-
 export default App;
